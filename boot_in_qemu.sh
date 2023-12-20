a0dd43d640bd4c04d9def85d478ac882440bf468
boli <bipinoli90@gmail.com>
2023-12-20 16:40:57 +0100

    updated cargo run to run bootloader

--- boot_in_qemu.sh
+++ boot_in_qemu.sh
@@ -1,1 +1,4 @@
+# "cargo run" has been configured to run bootloader (.cargo/config.toml)
+# so alternatively, you can simply run by "cargo run" command
+
 qemu-system-x86_64 -drive format=raw,file=target/x86_64_target/debug/bootimage-operating-system-from-scratch.bin
